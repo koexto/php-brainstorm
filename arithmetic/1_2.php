03843db0b797ab4c8c4286db234b72791f032201
koexto <boxkoe@gmail.com>
2019-07-16 14:00:32 +0300

    Add files via upload

---
+++ arithmetic/1_2.php
@@ -0,0 +1,22 @@
+<?php
+//Получить  все  четырехзначные  числа,  сумма  цифр  которых  равна заданному числу  n.
+$initialNumber = (int)readline('Number: ');
+
+for ($number=1000; $number < 10000 ; $number++) { 
+    $sum = sum($number);
+    if ($sum === $initialNumber) {
+        $numbers[] = $number;
+    }
+}
+
+var_dump($numbers);
+
+function sum($number)
+{
+    while( $number >= 1 ){
+        $sum += $number % 10;
+        $number = $number / 10;
+    }
+    return $sum;
+}
+
