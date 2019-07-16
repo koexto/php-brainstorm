7e890b7030046a8cad107c97d940af41a3b392a9
koexto <boxkoe@gmail.com>
2019-07-16 15:54:23 +0300

    Add files via upload

---
+++ arithmetic/1_4.php
@@ -0,0 +1,41 @@
+<?php
+//Найти все четные четырехзначные числа, цифры которых следуют в порядке возрастания или убывания.
+for ($number=1000; $number < 10000; $number+=2 ) { 
+    $digits = split($number);
+    if (isDecrease($digits) || isIncrease($digits)){
+        $numbers[] = $number;
+    }
+
+}
+
+var_dump($numbers);
+
+function isDecrease($digits)
+{
+    for ($i=0; $i < count($digits) - 1; $i++) {
+        if ($digits[$i+1] <= $digits[$i]){
+            return false;
+        }
+    }
+    return true;
+}
+
+function isIncrease($digits)
+{
+    for ($i=0; $i < count($digits) - 1; $i++) {
+        if ($digits[$i+1] >= $digits[$i]){
+            return false;
+        }
+    }
+    return true;
+}
+
+function split($number)
+{
+    while( $number >= 1 ){
+        $digits[] = $number % 10;
+        $number = $number / 10;
+    }
+    return $digits;
+}
+
