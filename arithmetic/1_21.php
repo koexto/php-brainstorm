cfc940f4493d1f3e3ab2916923cb4425f54f9442
koexto <boxkoe@gmail.com>
2019-07-19 16:40:06 +0300

    Add files via upload

---
+++ arithmetic/1_21.php
@@ -0,0 +1,27 @@
+<?php
+//Даны натуральные числа N и M. Определить, являются ли они взаимно простыми числами. Взаимно простые числа не имеют общих делителей, кроме единицы. 
+$n = (int)readline('N: ');
+$m = (int)readline('M: ');
+
+if (isDivisors($n, $m)){
+    echo 'no';
+}else{
+    echo 'yes';
+}
+
+function isDivisors($n, $m)
+{
+    if ($n % $m === 0 || $m % $n === 0){
+        return true;
+    }
+
+    for ($divisor=2; $divisor <= $n / 2; $divisor++) {
+        $remainder = $n % $divisor;
+        if ($remainder === 0){
+            if ($m % $divisor === 0){
+                return true;
+            }
+        }
+    }
+    return false;
+}
