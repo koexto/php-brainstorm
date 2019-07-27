cd90cd368d9109cbecb90a56332cf4c89e5d2af4
koexto <boxkoe@gmail.com>
2019-07-27 20:38:18 +0300

    Update 1_17.php

--- arithmetic/1_17.php
+++ arithmetic/1_17.php
@@ -1,27 +1,21 @@
 <?php
 //Найти наибольший общий делитель (НОД) двух натуральных чисел N и M.
-//пирамидальный код, давай сделаем что-нибудь более читабельное
 
 $n = readline('N: ');
 $m = readline('M: ');
 
-echo commonDivisor($n, $m);
+$gcd = $m > $n
+    ? $gcd = gcd($m, $n)
+    : $gcd = gcd($n, $m);
 
-function commonDivisor($n, $m)
+echo $gcd;
+
+function gcd($big, $small)
 {
-    if ($n % $m === 0){
-        return $m;
+    while ($big % $small != 0){
+        $remainder = $big % $small;
+        $big = $small;
+        $small = $remainder;
     }
-    if ($m % $n === 0){
-        return $n;
-    }
-
-    $divisor = (int)($n / 2);
-    for ($divisor; $divisor >= 1; $divisor--) {
-        if ($n % $divisor === 0){
-            if ($m % $divisor === 0){
-                return $divisor;
-            }
-        }
-    }
+    return $small;
 }
