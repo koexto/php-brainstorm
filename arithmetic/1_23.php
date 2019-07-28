e71bab1998cdd5b319ee007efdb93ae80bbeb0b4
koexto <boxkoe@gmail.com>
2019-07-28 21:06:36 +0300

    Update 1_23.php

--- arithmetic/1_23.php
+++ arithmetic/1_23.php
@@ -2,38 +2,36 @@
 //Дано целое число N. Преобразовать число так, чтобы его цифры следовали в порядке возрастания.  
 $number = (int)readline('N: ');
 
-//321
-//p i
-//1 2
-//
 $k=0;
 $increase = false;
-while ($k<5){
+while (!$increase){
     $increase = true;
     $prev = $number % 10;
     $number = $number / 10;
+    echo 'Number='.$number.PHP_EOL;
     $i = 0;
     while ($number >= 1) {
         $digit = $number % 10;
         //echo $digit.PHP_EOL;
-        if($prev <= $digit){
+        if($prev < $digit){
             $increase = false;
             $newNumber += 10**$i * $digit;
+            $last = $prev;
         }else{
             $newNumber += 10**$i * $prev;
             $prev = $digit;
-
+            $last = $digit;
         }
-
         $number = $number / 10;
         $i++;
+        echo $newNumber.PHP_EOL;
     }
-    $newNumber += 10**$i * $digit;
+    $newNumber += 10**$i * $last;
     echo 'newNumber='.$newNumber.PHP_EOL;
     $number = $newNumber;
+    $newNumber = 0;
     $k++;
 }
-
 
 
 
