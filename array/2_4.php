76ceff88d172a5d9430fdcbf9b99c3a5ea9b5285
koexto <boxkoe@gmail.com>
2019-08-05 15:45:43 +0300

    Add files via upload

--- array/2_4.php
+++ array/2_4.php
@@ -1,20 +1,11 @@
 <?php
 //В массиве А(N) найти количество пар одинаковых соседних элементов.
 
-$array = [2, 3, 4, 2, 4, 1, 44, 1, 44, 2, 12, 11];
+$array = [2, 3, 4, 4, 4, 1, 44, 1, 44, 2, 2, 11];
 
 for ($i=0; $i < count($array); $i++) { 
-    if (isIdentical($i, $array))
+    if ($array[$i] === $array[$i + 1])
         $sum += 1;
 }
 
 echo $sum;
-
-function isIdentical($i, $array)
-{
-    for ($k=$i + 1; $k < count($array); $k++) { 
-        if ($array[$i] === $array[$k])
-            return true;
-    }
-    return false;
-}
