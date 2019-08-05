eba73f814c2f512b108a16026b2006b799f7c366
koexto <boxkoe@gmail.com>
2019-08-05 16:23:35 +0300

    Add files via upload

---
+++ array/2_5.php
@@ -0,0 +1,24 @@
+<?php
+//В массиве А(N) найти максимальный и минимальный элементы и  переставить их местами.
+
+$array = [2, 3, 4, 124, 4, 1, 44, 1, 44, 2, 21, 11];
+$max = $array[0];
+$min = $array[0];
+$indexMax = 0;
+$indexMin = 0;
+
+for ($i=0; $i < count($array); $i++) { 
+    if ($max < $array[$i]){
+        $max = $array[$i];
+        $indexMax = $i;
+    }
+    if ($min > $array[$i]){
+        $min = $array[$i];
+        $indexMin = $i;
+    }
+}
+
+$array[$indexMax] = $min;
+$array[$indexMin] = $max;
+
+var_dump($array);
