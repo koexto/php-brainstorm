3fac6fe14fc0531e01f7b14173a0851679cf4b74
koexto <boxkoe@gmail.com>
2019-08-20 09:38:12 +0300

    Add files via upload

--- array/2_20.php
+++ array/2_20.php
@@ -1,2 +1,22 @@
 <?php
 //В  массиве  А(N)  выбрать  все  элементы,  встречающиеся  только  один раз.
+$array = [888, -3, 4, 124, -4, 1, 44, 44, 1, 444, 2, 2, 2, 2, 21, -11, 2];
+
+$amount = 0;
+for ($i = 0; $i < count($array); $i++){
+    if (isUnique($array, $i)){
+        $uniqueElements[] = $array[$i];
+    }
+}
+
+print_r($uniqueElements);
+
+function isUnique($array, $key)
+{
+    for ($i = 0; $i < count($array); $i++){
+        if ($array[$key] === $array[$i] && $key !== $i){
+            return false;
+        }
+    }
+    return true;
+}
